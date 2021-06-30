d6b2adf2e72b55bce1744690a881cdf0ea2ba83f
Sam Moore <swmoore84@gmail.com>
2021-06-30 23:05:13 +0900

    add event listener to button

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent = '3';
+// document.querySelector('.score').textContent = '30';
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if(!guess){
+    document.querySelector('.message').textContent = '⛔️ No number';
+  }
+})
