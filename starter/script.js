4305173216c4fdceceb8067329d154459cc3da3b
Sam Moore <swmoore84@gmail.com>
2021-07-01 23:52:53 +0900

    configure again button

--- starter/script.js
+++ starter/script.js
@@ -2,7 +2,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +13,7 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('.message').textContent = 'Correct number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
   } else if(guess > secretNumber){
     if(score > 1 ){
@@ -34,4 +34,32 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.score').textContent = 0;
     }
   }
+})
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector('.again').addEventListener('click', function() {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let score = 20;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = '';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 })
