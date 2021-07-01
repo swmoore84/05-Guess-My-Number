deeeb61d973cd1d7bf6c187d42660b5135245c54
Sam Moore <swmoore84@gmail.com>
2021-07-02 00:07:19 +0900

    update button

--- starter/script.js
+++ starter/script.js
@@ -57,7 +57,7 @@ document.querySelector('.again').addEventListener('click', function() {
 
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.score').textContent = '';
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = '#222';
