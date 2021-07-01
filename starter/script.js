845b1585d6a0ec72de180d645139391ad29b22a6
Sam Moore <swmoore84@gmail.com>
2021-07-01 23:11:30 +0900

    added rules for styling

--- starter/script.js
+++ starter/script.js
@@ -12,6 +12,9 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('.message').textContent = '⛔️ No number';
   } else if(guess === secretNumber){
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
   } else if(guess > secretNumber){
     if(score > 1 ){
       document.querySelector('.message').textContent = 'Too high';
