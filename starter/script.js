127ebe1d58c59d8c011d55b6c8c1ff0ad9a6ac5c
swmoore84 <swmoore84@gmail.com>
2021-07-02 14:12:54 +0900

    add high score

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +15,11 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore){
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
   } else if(guess > secretNumber){
     if(score > 1 ){
@@ -52,8 +58,8 @@ GOOD LUCK 😀
 */
 
 document.querySelector('.again').addEventListener('click', function() {
-  const secretNumber = Math.trunc(Math.random() * 20) + 1;
-  let score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
 
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
@@ -62,4 +68,4 @@ document.querySelector('.again').addEventListener('click', function() {
 
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-})
+});
